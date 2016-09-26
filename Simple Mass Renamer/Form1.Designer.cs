d8f4ec459ad45c25b555e265f62cdf4b450db443
Dabbles in too many things <mitch.zais@gmail.com>
2016-09-26 02:44:44 -0700

    Added Enter keypress to start Rename

--- Simple Mass Renamer/Form1.Designer.cs
+++ Simple Mass Renamer/Form1.Designer.cs
@@ -312,6 +312,7 @@
             // 
             // SimplerRenamer
             // 
+            this.AcceptButton = this.renameButton;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
